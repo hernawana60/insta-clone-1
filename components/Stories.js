72393cbc07ed9f825584c342dd9ee93f92c317e3
hernawana60 <hernawana60@gmail.com>
2021-10-25 23:15:19 +0700

    feed responsif

--- components/Stories.js
+++ components/Stories.js
@@ -17,7 +17,7 @@ function Stories() {
   }, []);
 
   return (
-    <div className="flex max-w-screen-sm space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+    <div className="flex w-screen space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
       {session && (
         <Story img={session.user.image} username={session.user.name} />
       )}
